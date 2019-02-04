cc66a4ddb8e4cf0311dd536679749f77176e46ea
Yasuto Nishii <yast@me.com>
2019-02-04 21:00:34 +0900

    parse integer

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nyasuto/monkey/ast"
 	"github.com/nyasuto/monkey/lexer"
 	"github.com/nyasuto/monkey/token"
+	"strconv"
 )
 
 const (
@@ -41,6 +42,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken()
 	p.nextToken()
@@ -136,6 +138,19 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 		p.nextToken()
 	}
 	return stmt
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
